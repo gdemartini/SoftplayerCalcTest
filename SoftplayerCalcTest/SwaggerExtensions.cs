29e88c86eb4ca9696871c3ca9b4512aeba3bf055
Giovani Demartini <giovani.demartini@dell.com>
2018-10-22 10:48:54 -0300

    Created project

---
+++ SoftplayerCalcTest/SwaggerExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SoftplayerCalcTest
+{
+  public static class SwaggerExtensions
+  {
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+      var thisAssembly = typeof(Program).Assembly;
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new Info { Version = "v1", Title = $"Softplayer Calc Test v{ thisAssembly.GetName().Version }" });
+      });
+
+      return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Softplayer Calc Test API v1");
+        c.RoutePrefix = string.Empty;
+
+        c.DocumentTitle = "Softplayer Calc Test API v1";
+        c.DocExpansion(DocExpansion.List);
+      });
+
+      return app;
+    }
+  }
+}
